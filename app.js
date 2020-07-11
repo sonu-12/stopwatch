d4ea7107829364abcce2350dd1c1c0ca6f971cde
sonu-12 <63183894+sonu-12@users.noreply.github.com>
2020-07-11 22:34:05 +0500

    stop watch

---
+++ app.js
@@ -0,0 +1,76 @@
+class Stopwatch {
+    constructor(display) {
+        this.running = false;
+        this.display = display;
+        
+        
+        this.reset();
+        this.print(this.times);
+    }
+    
+    reset() {
+        
+        this.times = [ 0, 0, 0 ];
+        
+    }
+
+    
+    start() {
+        if (!this.time) this.time = performance.now();
+        if (!this.running) {
+            this.running = true;
+            requestAnimationFrame(this.step.bind(this));
+        }
+    }
+    
+    
+    
+    stop() {
+        this.running = false;
+        this.time = null;
+    }
+
+    restart() {
+        if (!this.time) this.time = performance.now();
+        if (!this.running) {
+            this.running = true;
+            requestAnimationFrame(this.step.bind(this));
+        }
+        this.reset();
+    }
+    
+    clear() {
+        clearChildren(this.results);
+    }
+    
+    
+    
+
+    
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+    
+    format(times) {
+        return `\
+${pad0(times[0], 2)}:\
+${pad0(times[1], 2)}:\
+${pad0(Math.floor(times[2]), 2)}`;
+    }
+}
+
+function pad0(value, count) {
+    var result = value.toString();
+    for (; result.length < count; --count)
+        result = '0' + result;
+    return result;
+}
+
+function clearChildren(node) {
+    while (node.lastChild)
+        node.removeChild(node.lastChild);
+}
+
+let stopwatch = new Stopwatch(
+    document.querySelector('.stopwatch'),
+    document.querySelector('.results'));
